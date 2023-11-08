79ff36a02e581eacbd17f54530d9a7cb3bff0c4d
courtlynboykin <courtlynb@google.com>
2023-11-08 14:10:34 -0500

    Added two rows to one column for the quadrants

--- app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -3,14 +3,23 @@ package com.example.composequadrant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
@@ -21,92 +30,86 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeQuadrantTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    QuadrantOne(heading = "Text composable", info = "Displays text and follows the recommended Material Design guidelines.")
-                    QuadrantTwo(heading = "Image composable", info = "Creates a composable that lays out and draws a given Painter class object.")
-                    QuadrantThree(heading = "Row composable", info = "A layout composable that places its children in a horizontal sequence.")
-                    QuadrantFour(heading = "Column composable", info = "A layout composable that places its children in a vertical sequence.")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    FirstRow()
                 }
             }
         }
     }
-}
+
+
 
 @Composable
-fun QuadrantOne(heading: String, info: String, modifier: Modifier = Modifier) {
-    Column (
-        modifier = Modifier
-            .padding(16.dp)
+fun FirstRow() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            QuadrantText(
+                heading = stringResource(R.string.text_composable),
+                info = stringResource(R.string.text_info),
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            QuadrantText(
+                heading = stringResource(R.string.image_composable),
+                info = stringResource(R.string.image_info),
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            QuadrantText(
+                heading = stringResource(R.string.row_composable),
+                info = stringResource(R.string.row_info),
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            QuadrantText(
+                heading = stringResource(R.string.column_composable),
+                info = stringResource(R.string.column_info),
+                backgroundColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+    @Composable
+    fun QuadrantText(
+        heading: String,
+        info: String,
+        backgroundColor: Color,
+        modifier: Modifier = Modifier
     ) {
-        Text(
-            text = heading,
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = modifier
-        )
-        Text(
-            text = info,
-            modifier = modifier
-        )
+                .fillMaxSize()
+                .background(backgroundColor)
+                .padding(16.dp),
+
+        ){
+            Text(
+                text = heading,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(bottom = 16.dp)
+
+            )
+            Text(
+                text = info,
+                textAlign = TextAlign.Justify
+            )
+        }
+    }
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        ComposeQuadrantTheme {
+            FirstRow()
+        }
     }
 }
 
-@Composable
-fun QuadrantTwo(heading: String, info: String, modifier: Modifier = Modifier) {
-    Column (
-        modifier = Modifier
-            .padding(16.dp)
-    ) {
-        Text(
-            text = heading,
-            modifier = modifier
-        )
-        Text(
-            text = info,
-            modifier = modifier
-        )
-    }
-}
-
-@Composable
-fun QuadrantThree(heading: String, info: String, modifier: Modifier = Modifier) {
-    Column (
-        modifier = Modifier
-            .padding(16.dp)
-    ) {
-        Text(
-            text = heading,
-            modifier = modifier
-        )
-        Text(
-            text = info,
-            modifier = modifier
-        )
-    }
-}
-
-@Composable
-fun QuadrantFour(heading: String, info: String, modifier: Modifier = Modifier) {
-    Column (
-        modifier = Modifier
-            .padding(16.dp)
-    ) {
-        Text(
-            text = heading,
-            modifier = modifier
-        )
-        Text(
-            text = info,
-            modifier = modifier
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeQuadrantTheme {
-        QuadrantOne(heading = "Text composable", info = "Displays text and follows the recommended Material Design guidelines.")
-        QuadrantTwo(heading = "Image composable", info = "Creates a composable that lays out and draws a given Painter class object.")
-        QuadrantThree(heading = "Row composable", info = "A layout composable that places its children in a horizontal sequence.")
-        QuadrantFour(heading = "Column composable", info = "A layout composable that places its children in a vertical sequence.")
-    }
-}
